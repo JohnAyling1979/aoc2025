ba977f835458f0ac7cfce155feb0316cf646f40f
John Ayling <johnayling@yahoo.com>
2025-12-10 06:24:48 -0600

    Day 10 part 2 WIP.

--- day10/index.js
+++ day10/index.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('data', 'utf8');
+const input = fs.readFileSync('sample', 'utf8');
 
 const lines = input.split('\n').map(line => line.split(' '));
 
 const part1 = () => {
     let total = 0;
 
-    for (const line of lines) {
+    const part1Lines = JSON.parse(JSON.stringify(lines));
+
+    for (const line of part1Lines) {
         line.pop();
         const goal = line.shift().slice(1).slice(0, -1);
         const buttons = line;
@@ -50,7 +52,52 @@ const part1 = () => {
 }
 
 const part2 = () => {
-    return 0;
+    let total = 0;
+
+    const part2Lines = JSON.parse(JSON.stringify(lines));
+
+    const line = part2Lines[1];
+
+    // for (const line of part2Lines) {
+        line.shift();
+        const goal = line.pop().slice(1).slice(0, -1).split(',').join('');
+        const buttons = line;
+        const initialState = Array(goal.length).fill('0').join('');
+
+        const queue = [{state: initialState, pressCount: 0}];
+        const visited = new Set([initialState]);
+
+        console.log('intial', goal, initialState, buttons);
+
+        while (queue.length > 0) {
+            const {state, pressCount} = queue.shift();
+
+            console.log('checking', state, goal, pressCount);
+
+            if (state === goal) {
+                total += pressCount;
+                break;
+            }
+
+            for (const button of buttons) {
+                indexes = button.slice(1).slice(0, -1).split(',');
+                let newState = state.split('');
+
+                for (const index of indexes) {
+                    newState[index]++;
+                }
+
+                newState = newState.join('');
+
+                if (!visited.has(newState)) {
+                    queue.push({state: newState, pressCount: pressCount + 1});
+                    visited.add(newState);
+                }
+            }
+        }
+    // }
+
+    return total;
 }
 
 console.log(`Part 1: ${part1()}`);
